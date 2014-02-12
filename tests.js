97e3ee1db839b044c1a0a8d78fedeec2bb931b3f
DveMac <projects@milkjar.co.uk>
2014-02-12 16:58:15 +0000

    More basic tests

--- tests.js
+++ tests.js
@@ -1,18 +1,61 @@
-var Container = require('./index.js');
+var
+  should = require('should'),
+  Container = require('./index.js');
 
+// Some services
+function Formatter(a) {
+  this.injectedA = a;
+}
+Formatter.prototype.datePad = function (s) {
+  return (new Date()) + ': ' + s;
+}
 
-function Formatter() {}
-Formatter.prototype.datePad = function (s) { return (new Date()) + ': ' + s; }
-
-function MathService(formatter) { this.formatter = formatter; }
-MathService.prototype.add = function (a,b) { return this.formatter.datePad(a+b); };
+function MathService(formatter) {
+  this.formatter = formatter;
+}
+MathService.prototype.add = function (a, b) {
+  return a + b;
+};
+MathService.prototype.prettyAdd = function (a, b) {
+  return this.formatter.datePad(this.add(a, b));
+};
 MathService.$inject = ['FormattingService'];
 
 
+function A(b) {
+  this.b = b;
+}
+function B(a) {
+  this.a = a;
+}
+
+// the container
 var appContainer = new Container();
-appContainer.register('FormattingService', Formatter);
+
+// TODO: registrations with circular dependencies
+appContainer.register('A', A);
+//appContainer.register('B', B, ['A']);
+
+
+// registration of a class
+appContainer.register('FormattingService', Formatter, ['A']);
 appContainer.register('MathService', MathService);
 
+// registration of a function
+appContainer.register('Minus', function factory() { return function minus(a,b) { return a-b; } } );
+
+// registration of an object
+appContainer.register('AnObject', { whatdoyoulike: 'cats' });
+
+var formatter = appContainer.resolve('FormattingService');
+formatter.injectedA.should.be.ok;
+formatter.injectedA.should.eql(new A());
 
 var maths = appContainer.resolve('MathService');
-maths.add(2,5);
+maths.add(2, 5).should.eql(7);
+
+var minus = appContainer.resolve('Minus');
+minus(5,3).should.eql(2);
+
+var obj = appContainer.resolve('AnObject');
+obj.whatdoyoulike.should.eql('cats');
